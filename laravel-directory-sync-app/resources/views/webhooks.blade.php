4014f03ac367922ad1f82ce2aa0d5e5a826a3039
Edita Ignot <edignot@gmail.com>
2023-05-17 15:43:52 -0600

    dsync webhooks page updated

--- laravel-directory-sync-app/resources/views/webhooks.blade.php
+++ laravel-directory-sync-app/resources/views/webhooks.blade.php
@@ -1,59 +1,45 @@
 {{ Html::style('css/styles.css'); }}
 <script src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js"></script>
 
-
 <div class="logged_in_nav">
-        <div class="flex">
-            <div>
-            <img src="{{ URL::to('/images/workos-logo-with-text.png') }}"alt="workos logo">
-            </div>
-
-        </div>
+    <div class="flex">
         <div>
-            <a href="https://workos.com/docs" target="_blank"><button class='button nav-item'>Documentation</button></a>
-            <a href="https://workos.com/docs/reference" target="_blank"><button class='button nav-item'>API
-                    Reference</button></a>
-            <a href="https://workos.com/blog" target="_blank"><button
-                    class='button nav-item blog-nav-button'>Blog</button></a>
-            <a href="https://workos.com/" target="_blank"><button class='button button-outline'>WorkOS</button></a>
+            <img src="{{ URL::to('/images/workos-logo-with-text.png') }}" alt="workos logo">
         </div>
     </div>
-    <div class='flex'>
-        <div class="logged_in_div_left">
-            <div class="title-text">
-                <h1>Your app,</h1>
-                <h2 class="home-hero-gradient">Enterprise Ready</h2>
-            </div>
-            <div class="title-subtext">
-                <p>Start selling to enterprise customers with just a few lines of code.</p>
-                <p>Implement features like single sign-on in minutes instead of months.</p>
-            </div>
-            <div class="flex success-buttons">
-                <a href="https://workos.com/signup" target="_blank"><button class='button'>Get Started</button></a>
-                <a href="mailto:sales@workos.com?subject=WorkOS Sales Inquiry" target="_blank"><button
-                        class='button button-outline sales-button'>Contact
-                        Sales</button></a>
-            </div>
-        </div>
-    <div class="logged_in_div_right">
-        <div class="flex_column">
-            <h3>Send a webhook and watch in populate below</h3>
-            <div class="flex_column">
-                <pre class="prettyprint">
-                    <div class="text_box"></div>
-                    </div>
-                </pre>
-            </div>
-            <a href="{{ url('/') }}"><button class='button back'> ❮ Back </button></a>
-
-        </div>
+    <div class="flex">
+        <a href="https://workos.com/docs" target="_blank">
+            <button class='button nav-item'>Documentation</button>
+        </a>
+        <a href="https://workos.com/docs/reference" target="_blank">
+            <button class='button nav-item'>API Reference</button>
+        </a>
+        <a href="https://workos.com/blog" target="_blank">
+            <button class='button nav-item blog-nav-button'>Blog</button>
+        </a>
+        <a href="https://workos.com/" target="_blank">
+            <button class='button button-outline'>WorkOS</button>
+        </a>
     </div>
 </div>
-
-<script src="{{ asset('/js/app.js') }}"></script> 
-            <script> 
-                Echo.channel('home')
-                    .listen('NewMessage', (e) => {
-                        document.querySelector('.text_box').innerHTML = JSON.stringify(e.message)
-                    })
+<div class="logged_in_div_right">
+    <div id="webhooks-view-div" class="flex_column">
+        <div class="flex width-40vw space-between">
+            <h2>Live Webhooks View</h2>
+            <div id="tutorial_button">
+                <a href="https://workos.com/blog/test-workos-webhooks-locally-ngrok" target="_blank"
+                    class="button mt-15">Tutorial</a>
+                <a href="/webhooks" class="button mt-10">Clear</a>
+                <a href="{{ url('/') }}"><button class='button button-outline'>Back</button></a>
+            </div>
+        </div>
+        <div id="webhooks-view" class="width-40vw webhooks-container"></div>
+    </div>
+</div>
+<script src="{{ asset('/js/app.js') }}"></script>
+<script>
+    Echo.channel('home')
+        .listen('NewMessage', (e) => {
+            document.getElementById('webhooks-view').innerHTML = JSON.stringify(e.message);
+        });
 </script>
